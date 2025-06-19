84161bac83e5da265ddc52656a1c5cda0e5120ef
winjun1128 <hsj1128ty@naver.com>
2025-06-19 16:51:19 +0900

    조인  Join(INNER,OUTER)

---
+++ select07join.sql
@@ -0,0 +1,158 @@
+/***********************************/
+Join
+
+select * from emp;
+
+select * from dept;
+
+--JOIN 기준 deptno 가 같은!!
+
+--select empno, ename, dname, loc,deptno
+select e.empno, e.ename, d.dname, d.loc,d.deptno
+from emp e, dept d
+where e.deptno = d.deptno
+
+select e.empno, e.ename, d.dname, d.loc, d.deptno
+from emp e INNER JOIN dept d
+ON e.deptno = d.deptno;
+
+select *
+from emp e,dept d; --join 조건 누락->카티션 곱
+--where e.deptno = d.deptno;
+
+select * from student;
+select * from professor;
+--student <- join(profno) -> professor
+
+--select *
+select
+    s.name,p.name,s.studno,p.profno,p.email
+from student s, professor p
+where s.profno = p.profno;
+
+select
+    s.name,p.name,s.studno,p.profno,p.email
+from student s INNER JOIN professor p
+ON s.profno = p.profno;
+
+INNER JOIN, OUTER JOIN
+
+select COUNT(*) from student;
+select * from student;
+
+select *
+from student s, professor p
+--where s.profno = p.profno(+);
+--where p.profno = s.profno(+);
+where p.profno(+) = s.profno;
+
+select *
+from student s LEFT OUTER JOIN professor p
+--from student s RIGHT OUTER JOIN professor p
+--from professor p LEFT OUTER JOIN student s
+--from professor p RIGHT OUTER JOIN student s
+ON s.profno = p.profno;
+
+--professor 테이블 기준
+
+select COUNT(*) from professor; --16명
+
+select
+    s.name,p.name,s.studno,p.profno,p.email
+from student s, professor p
+where p.profno = s.profno;
+
+select
+    s.name,p.name,s.studno,p.profno,p.email
+from student s, professor p
+where p.profno = s.profno(+);
+
+
+select
+ s.name 학생이름,
+ p.profno 지도교수번호,
+ p.name 지도교수이름
+from student s, professor p
+where s.profno = p.profno;
+
+/******************************/
+select s.name 학생이름, s.profno 교수번호, p.name 교수이름
+from student s, professor p
+where s.profno = p.profno;
+
+select
+    s.name 학생이름,
+    d.dname 학생학과이름,
+    p.name 학생지도교수이름
+from student s,professor p,department d
+where s.deptno1 = p.deptno AND p.deptno = d.deptno;
+
+/*****************************/
+select * from department;
+
+select s.name STU_NAME, d.dname DEPT_NAME,p.name PROF_NAME
+from student s, professor p, department d
+where s.profno = p.profno
+AND s.deptno1 = d.deptno;
+
+select s.name STU_NAME, d.dname DEPT_NAME,p.name PROF_NAME
+from 
+    student s 
+    INNER JOIN professor p
+    ON s.profno = p.profno
+    INNER JOIN  department d
+    ON s.deptno1 = d.deptno;
+
+select s.name, p.profno,p.name
+from student s, professor p
+where s.deptno1 = 101
+AND s.profno = p.profno;
+
+--1) 학생명, 학생의 학과명, 교수명
+select s.name 학생명,s.deptno1 학생학과명,p.name 교수명
+from student s, professor p
+where s.profno = p.profno;
+/************************/
+select s.name,s.deptno1,d.dname,p.name,p.deptno
+from student s, professor p, department d
+where s.profno = p.profno
+AND d.deptno = s.deptno1;
+--2) 학생명, 교수의 학과명, 교수명
+select s.name 학생명,p.deptno 교수학과명,p.name 교수명
+from student s, professor p
+where s.profno = p.profno;
+/**********************/
+select s.name,s.deptno1,d.dname,p.name,p.deptno
+from student s, professor p,department d
+where s.profno = p.profno
+AND d.deptno = p.deptno;
+--AND s.deptno1<>p.deptno;
+--3)학생명,학생학과번호,학생의 학과명,교수명,교수학과번호,교수의 학과명
+select s.name,s.deptno1,ds.dname,p.name,p.deptno,dp.dname
+from student s,professor p,department dp,department ds
+where s.profno = p.profno
+AND dp.deptno = p.deptno --학과<->교수
+AND ds.deptno = s.deptno1; --학과<->학생
+/****************************************/
+비등가 Join
+
+select * from customer;
+select * from gift;
+
+select *
+from customer c, gift g
+where c.point BETWEEN g.g_start AND g.g_end;
+
+select * from hakjum;
+
+Self Join
+
+select * from emp;
+
+select *
+from emp e1, emp e2
+where e1.mgr = e2.empno;
+
+select A.empno 내사번,A.name 내이름,A.pempno 상사사번, B.name 상사이름
+from emp2 A, emp2 B
+where A.pempno = B.empno;
